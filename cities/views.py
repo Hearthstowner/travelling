f21424728ab32a5027bc02648a018e7014a23d7b
Hearthstowner <lazarev989@gmail.com>
2019-05-12 20:49:32 +0300

    Поменял погинацию с 2 до 10 в городах

--- cities/views.py
+++ cities/views.py
@@ -20,7 +20,7 @@ def home(request):
     # city = request.POST.get('name')
     # # print(city)
     cities = City.objects.all()
-    paginator = Paginator(cities, 2)
+    paginator = Paginator(cities, 10)
     page = request.GET.get('page')
     cities = paginator.get_page(page)
     return render(request, 'cities/home.html', {'objects_list': cities, })
